607e052b88d03fb5495fa2a7d67eb3fcd3721df7
void-hr <89070559+void-hr@users.noreply.github.com>
2023-12-09 04:16:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import { Routes, Route} from 'react-router-dom';
 function App() {
 
     const isHome = localStorage.getItem('fornData') ? true : false
-    const isCat = localStorage.getItem("categories") ? true : false
+   
   return (
    <Routes>
    <Route path='/' element= {isHome ? <HomePage/> : <RegistrationPage /> }/> 
     <Route path='/registration' element={isHome ? <HomePage/> : <RegistrationPage/>}/>
-    <Route path='/entertainment' element={isCat ? <EntertainmentPage/> : <CategoryPage />}/>
+    <Route path='/entertainment' element={<EntertainmentPage/>}/>
     <Route path='/category' element={<CategoryPage/>}/>
    </Routes>
   );
